e6eee519710cced9cb5467812994781ba74538c2
Luckyman909 <mingw2816@gmail.com>
2017-06-10 08:54:43 -0300

    Agreagada logica a la vista de bienvenida

--- app/Http/Controllers/BienvenidosController.php
+++ app/Http/Controllers/BienvenidosController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Criminal;
+use Illuminate\Support\Facades\Auth;
+use App\Pais;
 
 class BienvenidosController extends Controller
 {
     public function inicio(){
 
-        return View('bienvenida.inicio');
+        // Busco al criminal que tiene asignado el jugador
+        $criminal = Criminal::find(Auth::User()->idCriminal);
+
+        // Desarmo el array de los paises donde se encuentra
+        $array = explode(',', $criminal->ubicacion); 
+
+        // Busco la fila del primer pais que esta el ladron y le paso a la vista el nombre del pais.
+        $pais = Pais::find($array[0]);
+
+        return View('bienvenida.inicio', ['pais' => $pais->nombre]);
     }
 }
--- resources/views/bienvenida/inicio.blade.php
+++ resources/views/bienvenida/inicio.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts/master')  @section('titulo', 'Titulo')   @section('contenido')
 
-<h3 class="text-center">Bienvenido a la Jefatura Detective!!</h3>
+<div class="text-center">
+<h3>Bienvenido a la Jefatura Detective!!</h3>
 <img src="https://s-media-cache-ak0.pinimg.com/236x/a1/36/01/a13601d7bf323f2f9a6c396786c0aa20.jpg" />
-<h4 class="text-center">Ya se que es un novato, pero como soy un forro lo voy a mandar tras 5 mafias para que le rompan el culo</h4>
-<h5 class="text-center">El primer indicio para que se lo violen los de la mafia se encuentra en Argentina</h5>
-<h6 class="text-center">Tenes 1 mes de tiempo para desarmar toda la mafia o el que te va a romper el culo soy yo. <b>Con amor: El tipico jefe de policia negro con mal humor</h6>
-<a href="inicio" class="btn btn-warning">Ir a Argentina</a>
+<h5 class="text-center">Su primer caso es encontrar a un fulano, la ultima vez se lo vio en {{$pais}}. Yo comenzaria por ahi.</h5>
+<h6>Cuenta con un Mes de tiempo, o 720 Horas, para capturar al criminal. Cada pista que investigue le tomara una hora y cada pais que recorra lo hara perder 8 horas.</h6>
+<a href="inicio" class="btn btn-warning">Ir a {{$pais}}</a>
+</div>
 @endsection
